9d90b8a72a6a1c14e475864dba29ef9742ba4db8
James Estes <james.estes@gmail.com>
2015-09-01 11:22:06 -0600

    Back to 0.11.1 for doc updates.

---
+++ deploy-docs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e 
+
+VERSION=`grep version build.gradle | head -1 | tr -d "'" | awk '{print $NF}'`
+
+gradle clean javadoc
+
+cp -r build/docs/javadoc www/source/javadoc/${VERSION}
+git add . && git commit -am "Add javadocs for ${VERSION}"
+
+(cd www && middleman build)
+
+rm -rf /tmp/oio-java-client-docs-${VERSION}
+
+cp -r www/build /tmp/oio-java-client-docs-${VERSION}
+
+git checkout gh-pages
+git clean -fd
+cp -r /tmp/oio-java-client-docs-${VERSION}/* .
+
+git add . && git commit -am "Add javadocs for ${VERSION}"
+git push origin gh-pages:gh-pages
+
+git checkout master
+
